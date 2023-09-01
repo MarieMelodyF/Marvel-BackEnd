e8bda826e06387070e09f397792741fb6d42af0e
Marie Mélody F <mariemelo@mbp-de-marie.lan>
2023-09-01 16:56:56 +0200

    signup-model

--- models/User.js
+++ models/User.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 
 const User = mongoose.model("user", {
-  email: { type: String, required: true, unique: true },
+  email: { type: String, unique: true },
   username: { type: String, unique: true },
+  password: { type: String, unique: true },
   token: { type: String, unique: true },
   hash: { type: String, unique: true },
   salt: { type: String, unique: true },
