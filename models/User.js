b5931052f091bfb1df19c9451355d01619dce50c
Marie Mélody F <mariemelo@MacBook-Pro-de-Marie.local>
2023-09-02 12:36:51 +0200

    signup-erreur message

--- models/User.js
+++ models/User.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const User = mongoose.model(
   "User",
   mongoose.Schema({
-    email: { type: String, unique: true },
-    username: { type: String, unique: true },
+    email: { type: String, unique: true, require: true },
+    username: { type: String, unique: true, require: true },
     token: { type: String },
     hash: { type: String },
     salt: { type: String },
